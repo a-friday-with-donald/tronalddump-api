85e0e07fb124e578c40db931d5f3d5ceb8563a53
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-23 15:09:19 +0200

    Add tags into game logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./app.css";
 import Button from "./components/Button";
 import Header from "./components/Header";
+import { getRandomQuote, getAllTags } from "./utils/api";
 import { createElement, styled } from "./utils/elements";
 
 const PrimaryButton = styled(Button, "bg-primary");
@@ -21,6 +22,28 @@ function App() {
     ],
   });
 
+  const playerName = "Horst";
+
+  async function game() {
+    const quote = await getRandomQuote();
+    const tags = await getAllTags();
+    const answers = [];
+
+    if (quote.tags !== "") {
+      answers.push(quote.tags[0]);
+    }
+
+    let indexOld = tags.indexOf(quote.tags[0]);
+
+    while (answers.length < 3) {
+      const index = Math.floor(Math.random() * (tags.length + 1));
+      if (index !== indexOld) {
+        answers.push(tags[index]);
+        indexOld = index;
+      }
+    }
+  }
+  game();
   return container;
 }
 
