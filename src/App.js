9ff68dfd0311b6c6de4f3cf0858d3851a590d832
AlexisRoe <A.Roehrling@gmx.de>
2020-10-23 11:02:51 +0200

    Install localStorage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Button from "./components/Button";
 import Header from "./components/Header";
 import { createElement, styled } from "./utils/elements";
 
+import { getScore, storeScore, deletePlayer } from "./utils/localStorage";
+
 const PrimaryButton = styled(Button, "bg-primary");
 
 function App() {
@@ -20,6 +22,19 @@ function App() {
       PrimaryButton({ innerText: "World" }),
     ],
   });
+
+  const score = storeScore(1, 10);
+
+  console.log(`Player 1, saved score: ${score}`);
+
+  console.log(`test if data is in storage: ${getScore(1)}`);
+
+  const final= deletePlayer(1);
+  console.log(`delete player 1, successful: ${final}`);
+
+
+
+
   return container;
 }
 
