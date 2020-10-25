27f8f64ac16439c0a059366ceab6e3d91fa28f8d
AlexisRoe <A.Roehrling@gmx.de>
2020-10-25 03:52:49 +0100

    :bug: correct filename

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import "./Main.css";
+import { createElement } from "../utils/elements";
+
+export default function Main() {
+  
+  const main = createElement("main", {
+    className: "main"
+  })
+
+  return main;
+}
